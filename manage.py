691fab6a20d01c409b827682798807455aa3f3dc
Duncan Dewhurst <duncan.dewhurst@opendataservices.coop>
2023-08-14 17:29:54 +1200

    Various improvements
    - Add README sheet
    - Add codelist hyperlinks
    - Add data input guidance
    - Update error messages for array fields

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ from ocdskit.mapping_sheet import mapping_sheet
 from xlsxwriter.utility import xl_col_to_name
 
 RDLS_SCHEMA_URL = "https://raw.githubusercontent.com/GFDRR/rdl-standard/0__2__0/schema/rdls_schema.json"
+RDLS_DOCS_URL = "https://rdl-standard.readthedocs.io/en/dev"
 
 # https://flatten-tool.readthedocs.io/en/latest/create-template/#all-create-template-options
 TRUNCATION_LENGTH = 10
@@ -57,7 +58,6 @@ PALETTE = {
 # https://flatten-tool.readthedocs.io/en/latest/unflatten/#configuration-properties-skip-and-header-rows
 META_CONFIG = [
     "#",
-    "HeaderRows 7",
     "hashComments"
 ]
 
@@ -161,11 +161,16 @@ def create_template(component, schema_url):
     header_rows = {
         "path": {
             "row_height": None,
-            "cell_format": workbook.add_format({"bold": True, "bg_color": "#efefef"})
+            "cell_format": workbook.add_format({
+                "bold": True,
+                "bg_color": "#efefef"}),
+            "documentation": "A JSON pointer that identifies the RDLS field represented by the column. This information is used to convert data from spreadsheet format to JSON format. For more information, see https://flatten-tool.readthedocs.io/en/latest/unflatten/#understanding-json-pointer-and-how-flatten-tool-uses-it."
             },
         "title": {
             "row_height": None,
-            "cell_format": workbook.add_format({"bg_color": "#efefef"})
+            "cell_format": workbook.add_format({
+                "bg_color": "#efefef"}),
+            "documentation": "The title of the field, from the RDLS schema."
             },
         "description": {
             "row_height": 30,
@@ -174,15 +179,22 @@ def create_template(component, schema_url):
                 "text_wrap": True,
                 "valign": "top",
                 "bg_color": "#efefef"
-              })
+              }),
+            "documentation": "The description of the field, from the RDLS schema. You must ensure that the data you enter into each column conforms to the field's description."
             },
         "required": {
             "row_height": None,
-            "cell_format": workbook.add_format({"bg_color": "#efefef"})
+            "cell_format": workbook.add_format({
+                "font_size": 8,
+                "bg_color": "#efefef"}),
+            "documentation": "Whether the field is required (mandatory). You must populate required fields unless no other fields in the sheet are populated."
             },
         "type": {
             "row_height": None,
-            "cell_format": workbook.add_format({"bg_color": "#efefef"})
+            "cell_format": workbook.add_format({
+                "font_size": 8,
+                "bg_color": "#efefef"}),
+            "documentation": "The data type of the field, from the RDLS schema. Fields can be of type string (text), number (decimal), integer (whole number), boolean (true/false), or array (list of values)."
             },
         "values": {
             "row_height": 30,
@@ -191,16 +203,137 @@ def create_template(component, schema_url):
                 "text_wrap": True,
                 "valign": "top",
                 "bg_color": "#efefef"
-              })
+              }),
+            "documentation": "If the field references a closed codelist, its permissable values. For more information, see https://rdl-standard.readthedocs.io/en/dev/reference/codelists/. If the value of the field must conform to a particular string format, the name of the format. Fields can be of format date, email or iri (web address). For more information, see https://json-schema.org/understanding-json-schema/reference/string.html#built-in-formats."
             },
         "codelist": {
             "row_height": None,
-            "cell_format": workbook.add_format({"bottom": 1, "bg_color": "#efefef"})
-            }
+            "cell_format": workbook.add_format({
+                "font_size": 8,
+                "font_color": "blue",
+                "underline": True,
+                "bg_color": "#efefef"}),
+            "documentation": "If the field references a codelist, the name of the codelist. To view the title and description of each code, open the link."
+            },
+        "input guidance": {
+            "row_height": 50,
+            "cell_format": workbook.add_format({
+                "font_size": 8,
+                "text_wrap": True,
+                "valign": "top",
+                "bg_color": "#efefef",
+                "bottom": 1
+              }),
+            "documentation": "Guidance on how to enter data."            
+        }
     }
 
+    # Write readme worksheet
+    readme_worksheet = workbook.add_worksheet("# README")
+    readme_heading_format = workbook.add_format({
+              "bold": True,
+              "font_size": 18,
+              "valign": "top", 
+              })
+    readme_subheading_format = workbook.add_format({
+        "bold": True,
+        "font_size": 14,
+        "valign": "top"
+    })
+    readme_body_format = workbook.add_format({
+        "text_wrap": True,
+        "align": "left",
+        "valign": "top"
+    })
+
+    readme_content = [
+        {
+            "content": "RDLS spreadsheet input template",
+            "format": readme_heading_format,
+            "row_height": 30
+        },
+        {
+            "content": "A template for entering RDLS metadata in spreadsheet format",
+            "format": readme_body_format
+        },
+        {
+            "content": "",
+            "format": readme_body_format
+        },
+        {
+            "content": "Structure",
+            "format": readme_subheading_format,
+            "row_height": 30
+        },
+        {
+            "content": "This template consists of several worksheets, reflecting the structure of the RDLS schema. The `datasets` worksheet is the main worksheet and each row in the `datasets` worksheet represents a risk dataset. The other worksheets in the template represent arrays in the RDLS schema, with rows representing items in the arrays. For example, each row in the the `resources` worksheet represents represents a resource.",
+            "format": readme_body_format,
+            "row_height": 60
+        },
+        {
+            "content": "",
+            "format": readme_body_format
+        },
+        {
+            "content": "Relationships using identifiers",
+            "format": readme_subheading_format,
+            "row_height": 30
+        },
+        {
+            "content": "Identifiers are used to relate data entered across multiple worksheets, allowing the possibility of one-to-many relationships, such as one dataset made up of many resources. Rows in child worksheets are related to rows in parent worksheets using the parent object’s `id` field. For example, the `id` column in the `resources` is used to reference the `id` of the dataset to which the resource belongs. Similarly, each row in the `hazard_event_sets_events` represents an event in an event set. The `hazard/event_sets/0/id` column references the event set to which the event belongs and the `id` column references the dataset to which the event set and event belong.",
+            "format": readme_body_format,
+            "row_height": 90
+        },
+        {
+            "content": "",
+            "format": readme_body_format
+        },
+        {
+            "content": "Fields",
+            "format": readme_subheading_format,
+            "row_height": 30
+        },
+        {
+            "content": "Each column in the template represents a field in the RDLS schema. The following information is provided for each field:",
+            "format": readme_body_format,
+            "row_height": 30
+        },
+        {
+            "content": "",
+            "format": readme_body_format
+        }
+    ]
+
+    row_index = 0
+
+    for cell in readme_content:
+        readme_worksheet.write_row(row_index, 0, [cell["content"]], cell["format"])
+        readme_worksheet.set_row(row_index, cell.get("row_height"))
+        row_index +=1
+
+    readme_worksheet.write_row(
+        row_index,
+        0,
+        ["\n".join([f"{row_name}: {row_data['documentation']}\n\n" for row_name, row_data in header_rows.items()])],
+        workbook.add_format({
+            "valign": "top",
+            "text_wrap": True
+        })
+    )
+
+    readme_worksheet.set_row(row_index, 500)
+    readme_worksheet.set_column(0, 0, 100)
+
+    META_CONFIG.append(f"HeaderRows {len(header_rows)}"),
+
     # Add header column cell format
-    header_col_format = workbook.add_format({"bold": True, "font_size": 11, "bg_color": "#efefef"})
+    header_col_format = workbook.add_format({
+        "bold": True,
+        "font_size": 11,
+        "font_color": "black",
+        "underline": False,
+        "bg_color": "#efefef"
+        })
 
     # Generate sheet color mapping based on palette and sheet names
     sheet_colors = {key[:TRUNCATION_LENGTH]: value for key, value in PALETTE.items()}
@@ -269,11 +402,18 @@ def create_template(component, schema_url):
             metadata_path = "/".join([part for part in path.split("/")
                                      if part != '0'])
 
+            # Write field metadata as header rows
             data_type = field_metadata[metadata_path]["type"]
             values = field_metadata[metadata_path]["values"]
             codelist = field_metadata[metadata_path]["codelist"]
 
-            # Write field metadata as header rows
+            # Generate codelist hyperlink formula
+            if codelist:
+                codelist_name = codelist.split(".")[0]
+                codelist_formula = f"""=HYPERLINK("{RDLS_DOCS_URL}/reference/codelists/#{codelist_name.replace("_", "-")}","{codelist_name}")"""
+            else:
+                codelist_formula = ""
+
             metadata = {
                 "path": path,
                 "title": field_metadata[metadata_path]["title"],
@@ -281,8 +421,17 @@ def create_template(component, schema_url):
                 "required": "Required" if field_metadata[metadata_path]["range"][0] == "1" else "",
                 "type": data_type,
                 "values": values,
-                "codelist": codelist
+                "codelist": codelist_formula
             }
+
+            # Add data input guidance
+            if data_type == "array":
+                if values[:4] == 'Enum':
+                  metadata["input guidance"] = "Select from list or enter multiple values as a semicolon-separated list, e.g. a;b;c"
+                else:
+                  metadata["input guidance"] = "Enter multiple values as a semicolon-separated list, e.g. a;b;c"
+            else:
+                metadata["input guidance"] = ""
 
             worksheet.write_column(0, column, [metadata[row_name] for row_name in header_rows])
 
@@ -297,17 +446,17 @@ def create_template(component, schema_url):
                 cell_format = input_format
             
             # Write input cells, use formulae to populate links sheet
-            start_row = len(header_rows) + 1
+            input_row_ref = len(header_rows) + 1
             formulae = ["" for i in range(INPUT_ROWS)]
             if sheet_name == "links":
                 if path == "id":
-                    formulae = [f"={MAIN_SHEET_NAME}!B{i + start_row}" for i in range(INPUT_ROWS)]
+                    formulae = [f"={MAIN_SHEET_NAME}!B{i + input_row_ref}" for i in range(INPUT_ROWS)]
                 elif path == "links/0/href":
-                    formulae = [f'=IF(B{i + start_row}="","","{RDLS_SCHEMA_URL}")' for i in range(INPUT_ROWS)]
+                    formulae = [f'=IF(B{i + input_row_ref}="","","{RDLS_SCHEMA_URL}")' for i in range(INPUT_ROWS)]
                 elif path == "links/0/rel":
-                    formulae = [f'=IF(B{i + start_row}="","","describedby")' for i in range(INPUT_ROWS)]
+                    formulae = [f'=IF(B{i + input_row_ref}="","","describedby")' for i in range(INPUT_ROWS)]
             worksheet.write_column(
-                7, column, formulae, cell_format)
+                len(header_rows), column, formulae, cell_format)
 
             # Set column width
             worksheet.set_column(column, column, max(len(path), 16))
@@ -332,9 +481,13 @@ def create_template(component, schema_url):
 
                 if values[:4] == 'Enum':
                     codes = values[6:].split(", ")
-                    validation_options["error_type"] = "stop"
                     validation_options["error_title"] = "Value not in codelist"
-                    validation_options["error_message"] = "You must use a code from the codelist.\n\nIf no code is appropriate, please create an issue in the RDLS GitHub repository."
+                    if data_type == "array":
+                      validation_options["error_type"] = "warning"
+                      validation_options["error_message"] = "You must use a code from the codelist.\n\nIf no code is appropriate, please create an issue in the RDLS GitHub repository. If you entered multiple values from the codelist, you can ignore this warning."                        
+                    else:
+                      validation_options["error_type"] = "stop"
+                      validation_options["error_message"] = "You must use a code from the codelist.\n\nIf no code is appropriate, please create an issue in the RDLS GitHub repository."
                 else:
                     codelist_csv = get(
                         f"{schema_url.split('/rdls_schema.json')[0]}/codelists/open/{codelist}")
@@ -343,7 +496,10 @@ def create_template(component, schema_url):
                     codes = [row['Code'] for row in codelist_reader]
                     validation_options["error_type"] = "warning"
                     validation_options["error_title"] = "Value not in codelist"
-                    validation_options["error_message"] = "You must use a code from the codelist, unless no code is appropriate.\n\nIf you use new codes outside those in an open codelist, please create an issue in the RDLS GitHub repository, so that the codes can be considered for inclusion in the codelist."
+                    if data_type == "array":
+                        validation_options["error_message"] = "You must use a code from the codelist, unless no code is appropriate.\n\nIf you use new codes outside those in an open codelist, please create an issue in the RDLS GitHub repository, so that the codes can be considered for inclusion in the codelist. If you entered multiple values from the codelist, you can ignore this warning."
+                    else:
+                        validation_options["error_message"] = "You must use a code from the codelist, unless no code is appropriate.\n\nIf you use new codes outside those in an open codelist, please create an issue in the RDLS GitHub repository, so that the codes can be considered for inclusion in the codelist."
 
                 enum_worksheet.write_column(0, enum_column, [path] + codes)
                 enum_column_ref = xl_col_to_name(enum_column)
@@ -360,15 +516,15 @@ def create_template(component, schema_url):
             
             if validation_options:
                 worksheet.data_validation(
-                    7, column, 1007, column, validation_options)
+                    len(header_rows), column, 1007, column, validation_options)
 
             column += 1
 
     # Delete temp files
     delete_directory_contents(".temp")
 
-    # Write excel template
-    workbook.get_worksheet_by_name(MAIN_SHEET_NAME).activate()
+    # Write template to drive
+    readme_worksheet.activate()
     enum_worksheet.hide()
     workbook.get_worksheet_by_name("links").hide()
     workbook.close()
